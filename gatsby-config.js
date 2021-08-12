a1a69013f2040db57f79931dded250b875f60cfe
haleyelder <haley.elder@live.com>
2021-08-11 20:16:02 -0700

    removed icon

--- gatsby-config.js
+++ gatsby-config.js
@@ -117,7 +117,7 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/teal-icon.png`, // This path is relative to the root of the site.
+        // icon: `src/images/teal-icon.png`, // This path is relative to the root of the site.
       },
     },
     `gatsby-plugin-react-helmet`,
